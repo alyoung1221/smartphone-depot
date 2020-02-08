81bc289a2e8425007734f0958015b522af170a62
Alana Young <alana.young@unisys.com>
2020-02-07 20:47:03 -0500

    Removed featured section from index file

--- src/index.php
+++ src/index.php
@@ -9,37 +9,11 @@
 	<link rel="icon" href="images/favicon.png" type="image/x-icon">
 </head>
 <body>
-	<header>
-		<nav>
-			<ul>
-				<li><a href="index.php"><img src="images/logo.png" width="150px"></a></li>
-				<li><a href="inventory.php">Inventory</a></li>				
-				<li><a href="products.html">Products</a></li>
-				<li><a href="our-process.html">Our Process</a></li>
-				<li><a href="contact.html">Contact</a></li>
-			</ul>
-		</nav>	
-	</header>
-		<section id="featured">
-	<?php
-		$connection = mysqli_connect('helios.vse.gmu.edu','ayoung39','eesseg','ayoung39');
-			
-		if ($connection) {
-			if (mysqli_query($connection, "SELECT * FROM PHONES")) {				
-				$content = mysqli_query($connection, "SELECT PHONE_MODEL, PHONE_PRICE, PHONE_IMAGE FROM PHONES WHERE PHONE_FEATURED = 'F'");
-				$phone = mysqli_fetch_assoc($content);
-				
-				while ($phone) {
-					$phone = mysqli_fetch_assoc($content);					
-				}
-				mysqli_free_result($content);
-			}
-		}
-		else {
-			die('A database connection could not be established. The error was: '.mysqli_connect_error());
-		}				
-		mysqli_close($connection);
+	<?php 
+		readfile("header.html");
 	?>
-	</section>
+	<?php 
+		readfile("footer.html");
+	?>
 </body>
 </html>
