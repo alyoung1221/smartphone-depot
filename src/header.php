359101743cbea362504f59f3ff84c1bc5e81d4ec
Alana Young <60550038+alyoung1221@users.noreply.github.com>
2020-03-01 08:04:22 -0500

    Updated header

--- src/header.php
+++ src/header.php
@@ -1,20 +1,26 @@
+<?php 			
+	require_once("spwebsite/users/config.php");		
+?>
 <header>
-	<a href="index.php"><img src="images/logo.png"></a>
+	<a href="/"><img src="/images/logo.png"></a>
 	<button id="open" class="fas fa-bars mobile"></button>
 	<div class="menu-container">
 		<div class="menu">					
 			<button id="close" class="mobile">&times;</button>
 			<nav class="menu-content">
-				<div>
-					<a href="index.php" class="mobile">Home</a>
-					<a href="inventory.php">Inventory</a>
-					<a href="our-process.php">Our Process</a>
-					<a href="wholesale.php">Wholesale</a>
-					<a href="about-us.php">About Us</a>
-					<a href="contact-us.php">Contact Us</a>
-					<input type="search" id="search" placeholder="Search..." required title="">										
-					<a href="my-account.php" class="fas fa-user-alt"></a>	
+				<div class="menu-links">
+					<a href="/" class="mobile">Home</a>
+					<a href="/inventory">Inventory</a>
+					<a href="/our-process">Our Process</a>
+					<a href="/wholesale">Wholesale</a>
+					<a href="/about-us">About Us</a>
+					<a href="/contact-us">Contact Us</a>
+					<input type="search" id="search" placeholder="Search..." required title="Search website">										
 				</div>	
+				<div id="iconbar">
+					<a href="/my-account" class="material-icons acct" aria-label="My Account">person</a>
+					<a href="/cart" id="shopping-bag" aria-label="Cart"><span></span></a>
+				</div>
 			</nav>
 		</div>	
 	</div>		
