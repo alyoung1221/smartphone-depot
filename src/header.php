b60659a0944007ca48b55a99786efa06c3bd5e9e
Alana Young <60550038+alyoung1221@users.noreply.github.com>
2020-03-01 07:59:49 -0500

    Modified account page and styling

---
+++ src/header.php
@@ -0,0 +1,21 @@
+<header>
+	<a href="index.php"><img src="images/logo.png"></a>
+	<button id="open" class="fas fa-bars mobile"></button>
+	<div class="menu-container">
+		<div class="menu">					
+			<button id="close" class="mobile">&times;</button>
+			<nav class="menu-content">
+				<div>
+					<a href="index.php" class="mobile">Home</a>
+					<a href="inventory.php">Inventory</a>
+					<a href="our-process.php">Our Process</a>
+					<a href="wholesale.php">Wholesale</a>
+					<a href="about-us.php">About Us</a>
+					<a href="contact-us.php">Contact Us</a>
+					<input type="search" id="search" placeholder="Search..." required title="">										
+					<a href="my-account.php" class="fas fa-user-alt"></a>	
+				</div>	
+			</nav>
+		</div>	
+	</div>		
+</header>
