9b2cc6b0d3725b044c485f0e73b930bbf8d65f1b
esong8 <esong8@masonlive.gmu.edu>
2020-03-05 01:20:12 +0000

    my-account.php edited online with Bitbucket

--- src/my-account.php
+++ src/my-account.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 	<?php 
-		readfile("header.php");
+		readfile("header.html");
 	?>
 	<main>
 		<section id="acct">
@@ -21,34 +21,88 @@
 						<legend>Login</legend><br><br>
 						<label>Username or E-mail Address *</label><br><br>
 						<input type="text" name="loginID" required><br><br>
-						<label for="password">PASSWORD *</label><br><br>
+						<label for="password">Password *</label><br><br>
 						<input type="password" name="password" required><br><br>
 						<button type="submit" name="login">Log In</button>
 					</fieldset>
 				</form>
 				<form method="post" id="register">
 					<fieldset>
-						<legend>REGISTER</legend><br><br>
-						<label for="email">EMAIL ADDRESS *</label>
+						<legend>Register/legend<br><br>
+						<label for="email">Email Address *</label>
 						<input type="email" id="email" name="email" placeholder="E-mail*" title="Please enter your e-mail." maxlength="60"><br><br>
 							<p> A password will be sent to your email address </p> <br><br>
-							<label for="fname"> FIRST NAME * </label>
+							<label for="fname">First Name *</label>
 							<input type="text" id="fname" name="fname" placeholder="First Name" maxlength="50"><br><br>
-							<label for="lname"> LAST NAME* </label>
+							<label for="lname">Last Name *</label>
 							<input type="text" id="lname" name="lname" placeholder="Last Name" maxlength="50"><br><br>
-							<label  for="address"> ADDRESS 1 * </label>
+							<label for="address">Address 1 *</label>
 							<input type="text" id="address" name="address" placeholder="Address" maxlength="50"><br><br>
-							<label for="address2"> ADDRESS 2 </label>
+							<label for="address2">Address 2</label>
 							<input type="text" id="address" name="address" placeholder="Address 2" maxlength="50"><br><br>
-							<label for="city"> CITY * </label>
+							<label for="city">City *</label>
 							<input type="text" id="city" name="city" placeholder="City" maxlength="50"><br><br>
-							<label for="state"> State * </label> 
-							<input type="text" id="state" name="state" placeholder="State" maxlength="50"><br><br>
-							<label for="zip"> ZIP * </label>
+							<label>State: </label>
+							<!--<input type="city" name="city" class="form-control" value="">-->
+							<select class="state" name = "state">
+								<option value = "VA">- choose one -</option>
+								<option value = "DC">Dictrict of Columbia</option>
+									<option value = "AL">Alabama</option>
+									<option value = "AK">Alaska </option>
+									<option value = "AZ">Arizona </option>
+									<option value = "AR">Arkansas </option>
+									<option value = "CA">California </option>
+									<option value = "CO">Colorado </option>
+									<option value = "CT">Connecticut </option>
+									<option value = "DE">Delaware </option>
+									<option value = "FL">Florida </option>
+									<option value = "GA">Georgia </option>
+									<option value = "HI">Hawaii </option>
+									<option value = "ID">Idaho </option>
+									<option value = "IL">Illinois </option>
+									<option value = "IN">Indiana </option>
+									<option value = "IA">Iowa </option>
+									<option value = "KS">Kansas </option>
+									<option value = "KY">Kentucky </option>
+									<option value = "LA">Louisiana </option>
+									<option value = "ME">Maine </option>
+									<option value = "MD">Maryland </option>
+									<option value = "MA">Massachusetts </option>
+									<option value = "MI">Michigan </option>
+									<option value = "MN">Minnesota </option>
+									<option value = "MS">Mississippi </option>
+									<option value = "MO">Missouri </option>
+									<option value = "MT">Montana </option>
+									<option value = "NE">Nebraska</option>
+									<option value = "NV">Nevada </option>
+									<option value = "NH">New Hampshire</option>
+									<option value = "NJ">New Jersey </option>
+									<option value = "NM">New Mexico</option>
+									<option value = "NY">New York</option>
+									<option value = "NC">North Carolina</option>
+									<option value = "ND">North Dakota</option>
+									<option value = "OH">Ohio </option>
+									<option value = "OK">Oklahoma </option>
+									<option value = "OR">Oregon </option>
+									<option value = "PA">Pennsylvania </option>
+									<option value = "RI">Rhode Island</option>
+									<option value = "SC">South Carolina</option>
+									<option value = "SD">South Dakota</option>
+									<option value = "TN">Tennessee </option>
+									<option value = "TX">Texas </option>
+									<option value = "UT">Utah </option>
+									<option value = "VT">Vermont </option>
+									<option value = "VA">Virginia </option>
+									<option value = "WA">Washington </option>
+									<option value = "WV">West Virginia</option>
+									<option value = "WI">Wisconsin </option>
+									<option value = "WY">Wyoming </option>
+								</select><br><br>
+							<label for="zip">Zip *</label>
 							<input type="text" id="zip" name="zip" placeholder="Zipcode" maxlength="50"><br><br>
-							<label for="country"> COUNTRY * </label> 
+							<label for="country">County *</label> 
 							<input type="text" id="country" name="country" placeholder="Country" maxlength="50"><br><br>
-							<label for="phone"> PHONE * </label>
+							<label for="phone">Phone *</label>
 							<input type="text" id="phone" name="phone" placeholder="Phone Number" maxlength="50"><br><br>
 							<p>Your personal data will be used to support your experience throughout this website,
 							to manage access to your account, and for other purposes as described in our <a href="privacy-policy.html">privacy policy</a>.</p><br>
