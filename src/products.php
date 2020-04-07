473ada0bfedbabdad6f6565d56e47f2eea79fd54
esong8 <esong8@masonlive.gmu.edu>
2020-04-07 11:43:04 +0000

    products.php edited online with Bitbucket

--- src/products.php
+++ src/products.php
@@ -1,13 +1,18 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<title>Products | Smartphone Depot</title>
-	<link type="text/css" rel="stylesheet" href="css/lightslider.css"> 
-	<link href="css/styles.css" rel="stylesheet" type="text/css">
 	<link rel="icon" href="assets/favicon.png" type="image/x-icon">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+
 </head>
 <body>
 	<!-- Header -->
@@ -17,7 +22,42 @@
 	<!-- Main -->
 	<main>
 		<section>
-			<h1 tabindex="0">Products</h1>
+			<h1>Products</h1>
+            <div class="container">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="images/iPhone 6/gold/front.jpg" alt= "iphone 6" style="width:300px;">
+                    </div>
+
+                    <div class="item">
+                        <img src="images/iPhone 7/matte black/front.jpg" alt= "iphone 7" style="width:150px;">
+                    </div>
+                    
+                    <div class="item">
+                        <img src="images/iPhone X/white/front.jpg" alt= "iphone X" style="width:300px;"> 
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+                </a>
+            </div>
+            </div>
 			<div class="gallery">
 				<?php
 					if (mysqli_query($link, "SELECT * FROM PHONES")) {
